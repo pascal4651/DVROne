13f236f02b60faf46bbb298a0dee94ab4fa7befe
xuviuz <kristiangottschalk@gmail.com>
2018-05-01 12:32:55 +0200

    endnu en tilladelse til android 6+

--- app/src/main/java/com/example/alex/dvrone/MainActivity.java
+++ app/src/main/java/com/example/alex/dvrone/MainActivity.java
@@ -74,6 +74,11 @@ public class MainActivity extends AppCompatActivity {
                         new String[]{Manifest.permission.CAMERA},
                         1);
             }
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            }
 
         }
 
