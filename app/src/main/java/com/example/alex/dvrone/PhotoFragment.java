a2cebba842fcf845efd425d0f30a15a29d569baf
Oleksandr Shvets <pascal36@yandex.com>
2018-05-14 13:24:59 +0200

    deleted headers in fragments

--- app/src/main/java/com/example/alex/dvrone/PhotoFragment.java
+++ app/src/main/java/com/example/alex/dvrone/PhotoFragment.java
@@ -1,6 +1,5 @@
 package com.example.alex.dvrone;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
--- app/src/main/java/com/example/alex/dvrone/VideoFragment.java
+++ app/src/main/java/com/example/alex/dvrone/VideoFragment.java
@@ -1,12 +1,6 @@
 package com.example.alex.dvrone;
 
-import android.content.Context;
 import android.content.Intent;
-<<<<<<< HEAD
-=======
-import android.net.Uri;
-import android.os.AsyncTask;
->>>>>>> f5f5b858f2d76e373bbf98108453c27c00cb0a8f
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
@@ -27,7 +21,6 @@ public class VideoFragment extends Fragment {
     private String[] fileNames;
     private View view;
     private static int currentIndex;
-    private GridView gridview;
 
     public VideoFragment() {
         // Required empty public constructor
@@ -71,16 +64,7 @@ public class VideoFragment extends Fragment {
         File directory = new File(path);
 
         files = directory.listFiles();
-<<<<<<< HEAD
-        fileNames = new String[files.length];
-        Log.d("Files", "Size: "+ files.length);
-        for (int i = 0; i < files.length; i++)
-        {
-            fileNames[i] = files[i].getName();
-        }
-        gridview = (GridView) view.findViewById(R.id.gridview);
-        //gridview.setAdapter(new VideoAdapter(this.getContext(),files,fileNames));
-=======
+
         if(files != null) {
             fileNames = new String[files.length];
             Log.d("Files", "Size: " + files.length);
@@ -89,19 +73,13 @@ public class VideoFragment extends Fragment {
             }
             GridView gridview = (GridView) view.findViewById(R.id.gridview);
             gridview.setAdapter(new VideoAdapter(this.getContext(), files, fileNames));
->>>>>>> f5f5b858f2d76e373bbf98108453c27c00cb0a8f
 
             gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View v,
                                         int position, long id) {
 
-<<<<<<< HEAD
-                Toast.makeText(getContext(), "" + files[position].getAbsolutePath(), Toast.LENGTH_SHORT).show();
-                currentIndex = position;
-=======
-                    Toast.makeText(con, "" + files[position].getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "" + files[position].getAbsolutePath(), Toast.LENGTH_SHORT).show();
                     currentIndex = position;
->>>>>>> f5f5b858f2d76e373bbf98108453c27c00cb0a8f
 
                     Intent intent = new Intent(getActivity(), GalleryActivityVideo.class);
                     startActivity(intent);
